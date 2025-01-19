108d131c0c071dec032547ad01acb4e9b952b271
yassine snaiki <yassnaiki@gmail.com>
2025-01-19 20:52:19 +0100

    v

--- models/Admin.php
+++ models/Admin.php
@@ -8,12 +8,4 @@ class Admin extends User {
     public function __construct($id = null, $email, $password, $username, $isactive = true) {
         parent::__construct($id, $email, $password, $username, 'admin', $isactive);
     }
-
-    public function validateTeacherAccount($teacher_id) {
-        Application::$app->db->query("UPDATE users SET role = 'teacher' WHERE id = ?", [$teacher_id]);
-    }
-
- 
-
-
 }
